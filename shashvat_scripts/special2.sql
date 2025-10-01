2bea91aedc75fc688d320eafb7efdde6b562b03f
Shashvat Chathapuram <shashvatc@hotmail.com>
2025-09-30 22:51:42 -0500

    Added a comment in special2 for running script

--- shashvat_scripts/special2.sql
+++ shashvat_scripts/special2.sql
@@ -1,7 +1,7 @@
 /* pseudocode: select count of orders, sum of order total grouped by hour
 about: given a specific hour of the day, how many orders were placed and what was the total sum of the orders?
 example: e.g., "12pm has 12345 orders totaling $86753" */
-
+-- Connect to server and run: \i 'shashvat_scripts/special2.sql'
 SELECT
     EXTRACT(HOUR FROM order_time) AS hour_of_day, 
 
