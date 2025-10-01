be213fcc6287906cef27b8b9a4cb5448a6f6923f
Shashvat Chathapuram <shashvatc@hotmail.com>
2025-09-30 22:30:21 -0500

    Finished scripts 4,5,6 and special2

--- shashvat_scripts/script5.sql
+++ shashvat_scripts/script5.sql
@@ -1,1 +1,3 @@
-what is the price of mango fruit tea
+/* what is the price of mango fruit tea */
+-- Connect to server and run: \i 'shashvat_scripts/script5.sql'
+SELECT price FROM Menu WHERE item_name = 'Mango Fruit Tea';
--- shashvat_scripts/script6.sql
+++ shashvat_scripts/script6.sql
@@ -1,1 +1,3 @@
-what is the price of the popcorn chicken
+/* what is the price of the popcorn chicken */
+-- Connect to server and run: \i 'shashvat_scripts/script6.sql'
+SELECT price FROM Menu WHERE item_name = 'Popcorn Chicken';
